b9c13b02504099375cc3e01f5e877a80d64325c4
Michael Mandel <mim@mr-pc.org>
2014-08-19 10:14:20 -0400

    Optimized dist, especially for repeated calls on one argument

--- dist.m
+++ dist.m
@@ -1,7 +1,7 @@
-function d = dist(X1, X2)
+function [d K11 K22] = dist(X1, X2, K11, K22)
 
-% d = ipDist(X1)
-% d = ipDist(X1, X2)
+% d = dist(X1)
+% d = dist(X1, X2)
 %
 % Find the euclidean distance between X1 and X2 by calculating
 % their inner products and then manipulating the resulting matrix.
@@ -11,29 +11,31 @@ function d = dist(X1, X2)
 % Copyright (C) 2005 Michael Mandel, mim at ee columbia edu;
 % distributable under GPL
 
+if ~exist('K11', 'var'), K11 = []; end
+if ~exist('K22', 'var'), K22 = []; end
 
 % d_ij = sqrt(x_i*x_i - 2*x_i*x_j + x_j*x_j) 
 %      = sqrt(K(i,i) - 2*K(i,j) + K(j,j))
 
-if(nargin < 2)
+if ~exist('X2', 'var') || isempty(X2)
   % If it's the matrix with itself, we can take some shortcuts
-  [N,D] = size(X1);
-  
   K = X1 * X1';
-  Kii = repmat(diag(K), 1, N);
+  K11 = diag(K);
   
-  d = Kii - 2*K + Kii';
+  d = bsxfun(@plus, bsxfun(@plus, K11, -2*K), K11');
   d = sqrt(d .* (d > 0));
 
 else
   % Otherwise, we have to do it the long way
-  [N,D] = size(X1);
-  [M,D2] = size(X2);
+  if isempty(K11)
+      K11 = sum(X1 .* X1, 2);
+  end
+  if isempty(K22)
+      K22 = sum(X2 .* X2, 2)';
+  end
   
   K = X1 * X2';
-  Kii = repmat(sum(X1 .* X1, 2), 1, M);
-  Kjj = repmat(sum(X2 .* X2, 2)', N, 1);
   
-  d = Kii - 2*K + Kjj;
+  d = bsxfun(@plus, bsxfun(@plus, K11, -2*K), K22);
   d = sqrt(d .* (d > 0));
 end
