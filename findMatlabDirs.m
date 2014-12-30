327a80816804b46454b6e037764ab974020cd87f
Michael Mandel <mim@mr-pc.org>
2014-12-30 14:04:18 -0500

    adjusted order of matlab directory finding

--- findMatlabDirs.m
+++ findMatlabDirs.m
@@ -10,12 +10,15 @@ else
 end
 
 if findDirsWithMFiles
-    [~,mFiles] = findFiles(basedir, '\.m$', 1, ignorePattern);
+    [~,mFiles2] = findFiles(fullfile(basedir, 'lib'), '\.m$', 1, ignorePattern);
+    [~,mFiles3] = findFiles(fullfile(basedir, 'github'), '\.m$', 1, ignorePattern);
+    [~,mFiles1] = findFiles(fullfile(basedir, 'matlab'), '\.m$', 1, ignorePattern);
+    mFiles = [mFiles1; mFiles2; mFiles3];
     dirs = listMap(@(x) fileparts(x), mFiles);
     dirs = unique(dirs);
 else
-    [~,dirs1] = findFiles(fullfile(basedir, 'matlab'), '.', 0, ignorePattern);
     [~,dirs2] = findFiles(fullfile(basedir, 'lib'), '.', 0, ignorePattern);
     [~,dirs3] = findFiles(fullfile(basedir, 'github'), '.', 0, ignorePattern);
+    [~,dirs1] = findFiles(fullfile(basedir, 'matlab'), '.', 0, ignorePattern);
     dirs = [dirs1; dirs2; dirs3];
 end
