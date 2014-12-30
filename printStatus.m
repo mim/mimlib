2ab085ee2d96a44605a232976e0efa5390d4e468
Michael Mandel <mim@mr-pc.org>
2014-12-30 14:04:58 -0500

    print a status character with smart line wrapping

---
+++ printStatus.m
@@ -0,0 +1,17 @@
+function printStatus(c)
+
+% Print a character as a status update on the console, intelligently wrap
+% lines by remembering how many have been printed.  Start over by calling
+% with c = '\n';
+
+maxCol = 80;
+persistent col;
+if isempty(col) || strcmp(c, '\n')
+    col = 0;
+end
+fprintf(c)
+col = col + 1;
+if col >= maxCol
+    fprintf('\n')
+    col = mod(col, maxCol);
+end
