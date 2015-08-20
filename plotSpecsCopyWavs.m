1c52f9d466502668c38141262ed4d2aebbb7b523
Michael Mandel <mim@mr-pc.org>
2015-08-20 17:26:47 -0400

    window divisible by 2, labels can be specified as arguments

--- plotSpecsCopyWavs.m
+++ plotSpecsCopyWavs.m
@@ -1,4 +1,4 @@
-function plotSpecsCopyWavs(inDir, outDir, toDisk, startAt, files, cax, maxFreq_hz, xrange_s, varargin)
+function plotSpecsCopyWavs(inDir, outDir, toDisk, startAt, files, cax, maxFreq_hz, xrange_s, labels, varargin)
 
 if ~exist('files', 'var'), files = {}; end
 if ~exist('toDisk', 'var') || isempty(toDisk), toDisk = 0; end
@@ -6,6 +6,7 @@ if ~exist('startAt', 'var') || isempty(startAt), startAt = 0; end
 if ~exist('maxFreq_hz', 'var') || isempty(maxFreq_hz), maxFreq_hz = inf; end
 if ~exist('xrange_s', 'var'), xrange_s = []; end
 if ~exist('cax', 'var') || isempty(cax), cax = [-80 10]; end
+if ~exist('labels', 'var') || isempty(labels), labels = [1 1 1]; end
 
 prt('ToFile', toDisk, 'StartAt', startAt, 'NumberPlots', 0, ...
     'Width', 4, 'Height', 3, 'TargetDir', outDir, ...
@@ -23,7 +24,7 @@ for f = 1:length(files)
     if ~exist(inFile, 'file'), continue; end
     
     [X fs hop_s] = loadSpecgram(inFile);
-    prtSpectrogram(db(X), name, fs, hop_s, cmap, cax, [1 1 1], maxFreq_hz, xrange_s, varargin{:})
+    prtSpectrogram(db(X), name, fs, hop_s, cmap, cax, labels, maxFreq_hz, xrange_s, varargin{:})
 
     outWavFile = fullfile(outDir, files{f});
     if toDisk
@@ -39,7 +40,7 @@ if ~exist('win_s', 'var') || isempty(win_s), win_s = 0.032; end
 
 [x fs] = wavReadBetter(file);
 x = x(:,1);
-nfft = round(win_s * fs);
+nfft = 2 * round(win_s * fs / 2);
 hop = round(nfft / 4);
 X = stft(x', nfft, nfft, hop);
 hop_s = hop / fs;
