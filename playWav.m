a3b7e84736f7728e1f325b59738ae1f9955de392
Michael Mandel <mim@mr-pc.org>
2015-08-20 17:26:08 -0400

    show spectrograms of files being played

--- playWav.m
+++ playWav.m
@@ -1,11 +1,24 @@
-function playWav(wavs)
+function playWav(wavs, showSpec, doPause, chan)
+
+if ~exist('showSpec', 'var') || isempty(showSpec), showSpec = false; end
+if ~exist('doPause', 'var') || isempty(doPause), doPause = true; end
+if ~exist('chan', 'var') || isempty(chan), chan = 1; end
 
 if ~iscell(wavs)
     wavs = {wavs};
 end
 
 for i = 1:length(wavs)
-    fprintf('%s\n', wavs{i});
+    fprintf('%d: %s channel %d\n', i, wavs{i}, chan);
     [x fs] = wavread(wavs{i});
-    sound(x, fs);
+    if showSpec
+        X = stft(x(:,chan)', 1024, 1024, 256);
+        subplots(db(X));
+    end
+    
+    sound(x(:,chan), fs);
+    
+    if doPause
+        pause
+    end
 end
