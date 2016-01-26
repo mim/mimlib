9552893b5a923919041e4b443d33d54c06214f50
Michael Mandel <mim@mr-pc.org>
2016-01-26 14:34:18 -0500

    made asynchronous playback synchronous

--- playWav.m
+++ playWav.m
@@ -1,8 +1,10 @@
-function playWav(wavs, showSpec, doPause, chan)
+function playWav(wavs, ends_s, showSpec, doPause, chan, cax)
 
+if ~exist('ends_s', 'var'), ends_s = []; end
 if ~exist('showSpec', 'var') || isempty(showSpec), showSpec = false; end
 if ~exist('doPause', 'var') || isempty(doPause), doPause = true; end
 if ~exist('chan', 'var') || isempty(chan), chan = 1; end
+if ~exist('cax', 'var'), cax = []; end
 
 if ~iscell(wavs)
     wavs = {wavs};
@@ -10,15 +12,33 @@ end
 
 for i = 1:length(wavs)
     fprintf('%d: %s channel %d\n', i, wavs{i}, chan);
-    [x fs] = audioread(wavs{i});
+    
+    info = audioinfo(wavs{i});
+    fs = info.SampleRate;
+    
+    if isempty(ends_s)
+        [x fs] = audioread(wavs{i});
+    else
+        ends = ends_s * fs;
+        ends(1) = ends(1) + 1;
+        [x fs] = audioread(wavs{i}, ends);
+    end
+        
+        
     if showSpec
         X = stft(x(:,chan)', 1024, 1024, 256);
-        subplots(db(X));
+        if isempty(cax)
+            subplots(db(X));
+        else
+            subplots(lim(db(X), cax(1), cax(2)));
+        end
     end
     
     sound(x(:,chan), fs);
     
     if doPause
         pause
+    else
+        pause(size(x,1)/fs)
     end
 end
